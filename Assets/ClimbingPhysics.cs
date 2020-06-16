006badaf04d8beb061a2e19cad5e188aee1c2c13
Jonathan Peters <jonathan.peters99@gmail.com>
2020-06-16 10:48:55 -0700

    Added new climbing physics
    Bugs: jittering due to heading change, moving too fast to go over corner.

--- Assets/ClimbingPhysics.cs
+++ Assets/ClimbingPhysics.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class ClimbingPhysics : MonoBehaviour
 {
     public StacsEntity entity;
+    public float magneticForce;
+    public float gravityForce;
+    public float groundCheckDistance;
+
+    Vector3 groundNormal;
+    float force;
 
     private void Awake()
     {
         entity = GetComponentInParent<StacsEntity>();
         entity.position = transform.localPosition;
+
     }
     // Start is called before the first frame update
     void Start()
@@ -23,11 +30,16 @@ public class ClimbingPhysics : MonoBehaviour
     void Update()
     {
         //speed
-        if (Utils.ApproximatelyEqual(entity.speed, entity.desiredSpeed)) {
+        if (Utils.ApproximatelyEqual(entity.speed, entity.desiredSpeed))
+        {
             ;
-        } else if (entity.speed < entity.desiredSpeed) {
+        }
+        else if (entity.speed < entity.desiredSpeed)
+        {
             entity.speed = entity.speed + entity.acceleration * Time.deltaTime;
-        } else if (entity.speed > entity.desiredSpeed) {
+        }
+        else if (entity.speed > entity.desiredSpeed)
+        {
             entity.speed = entity.speed - entity.acceleration * Time.deltaTime;
         }
         entity.speed = Utils.Clamp(entity.speed, entity.minSpeed, entity.maxSpeed);
@@ -35,24 +47,65 @@ public class ClimbingPhysics : MonoBehaviour
         //altitude
         entity.altitude = entity.desiredAltitude = 0;
         //heading
-        if (Utils.ApproximatelyEqual(entity.heading, entity.desiredHeading)) {
+        if (Utils.ApproximatelyEqual(entity.heading, entity.desiredHeading))
+        {
             ;
-        } else if (Utils.AngleDiffPosNeg(entity.desiredHeading, entity.heading) > 0) {
+        }
+        else if (Utils.AngleDiffPosNeg(entity.desiredHeading, entity.heading) > 0)
+        {
             entity.heading += entity.turnRate * Time.deltaTime;
-        } else if (Utils.AngleDiffPosNeg(entity.desiredHeading, entity.heading) < 0) {
+        }
+        else if (Utils.AngleDiffPosNeg(entity.desiredHeading, entity.heading) < 0)
+        {
             entity.heading -= entity.turnRate * Time.deltaTime;
         }
         entity.heading = Utils.Degrees360(entity.heading);
         //
-        entity.velocity.x = Mathf.Sin(entity.heading * Mathf.Deg2Rad) * entity.speed;
-        entity.velocity.y = 0;
-        entity.velocity.z = Mathf.Cos(entity.heading * Mathf.Deg2Rad) * entity.speed;
 
-        entity.position = entity.position + entity.velocity * Time.deltaTime;
-        //entity.position.y = entity.altitude;
-        transform.localPosition = entity.position;
-
+        transform.rotation = Quaternion.LookRotation(groundNormal, -transform.forward);
+        transform.Rotate(Vector3.right, 90f);
+        eulerRotation = transform.localEulerAngles;
         eulerRotation.y = entity.heading;
         transform.localEulerAngles = eulerRotation;
+
+
+        ApplyPhysics();
+    }
+
+    public void ApplyPhysics()
+    {
+        CheckGroundStatus();
+        entity.velocity.z = entity.speed;
+        entity.velocity.y = transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity).y + ((force / entity.mass) * Time.deltaTime);
+        Debug.Log("local velocity: " + transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity));
+        GetComponent<Rigidbody>().velocity = transform.TransformDirection(entity.velocity);
+    }
+
+    void CheckGroundStatus()
+    {
+        RaycastHit hitInfo;
+#if UNITY_EDITOR
+        // helper to visualise the ground check ray in the scene view
+        Debug.DrawLine(transform.position + (transform.up * 0.1f), transform.position + (transform.up * 0.1f) + (-transform.up * groundCheckDistance));
+#endif
+        // 0.1f is a small offset to start the ray from inside the character
+        // it is also good to note that the transform position in the sample assets is at the base of the character
+        if (Physics.Raycast(transform.position + (transform.up * 0.1f), -transform.up, out hitInfo, groundCheckDistance))
+        {
+            groundNormal = hitInfo.normal;
+            if (hitInfo.collider.gameObject.tag == "Truss")
+            {
+                force = magneticForce;
+            }
+            else
+            {
+                force = gravityForce;
+            }
+        }
+        else
+        {
+            groundNormal = Vector3.up;
+            force = gravityForce;
+        }
     }
 }
