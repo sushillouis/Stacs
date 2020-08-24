be9d11ad8d548589a1018fe10e42c94bf0c9e308
Jonathan Peters <jonathan.peters99@gmail.com>
2020-08-24 11:42:11 -0700

    DefectManager
    Created a DefectManager which spawns the specified number of defects randomly at predefined spawn locations. The appearance of the defects is temporary and the spawn points are temprorary.

---
+++ Assets/DefectManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefectManager : MonoBehaviour
+{
+    public Transform defectLocationsRoot;
+    public List<Transform> defectLocations;
+    public GameObject defect;
+    public int numDefects;
+
+    void Start()
+    {
+        foreach(Transform t in defectLocationsRoot.GetComponentsInChildren<Transform>())
+        {
+            if(t == defectLocationsRoot)
+                continue;
+            defectLocations.Add(t);
+        }
+
+        bool[] occupiedSpaces = new bool[defectLocations.Count];
+        for(int i = 0; i < occupiedSpaces.Length; i++)
+        {
+            occupiedSpaces[i] = false;
+        }
+
+        for(int i = 0; i < numDefects; i++)
+        {
+            int loc;
+            do
+            {
+                loc = Random.Range(0, defectLocations.Count);
+            } while(occupiedSpaces[loc] == true);
+
+            Instantiate(defect, defectLocations[loc]);
+            occupiedSpaces[loc] = true;
+        }
+    }
+}
